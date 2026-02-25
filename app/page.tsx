e4776bb121c04dbd5e2d19c921647e262d0cbb34
StefanoMela <stefano.mela25@gmail.com>
2026-02-25 13:29:46 +0100

    refac design system and structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,15 @@
-import data from "./data/data.json";
-import Image from "next/image";
-
-interface Destination {
-  name: string;
-  image: string
-  description: string;
-  distance: string;
-  travel: string;
-}
-
-interface Technology {
-  name: string;
-  image: string;
-  description: string;
-}
-
-
-
-export default function Home() {
-
+export default function Index() {
   return (
-    <div>
-      <h1>Space Tourism</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-h1">Space Tourism</h1>
+      <h2 className="text-h2">Space Tourism</h2>
+      <h3 className="text-h3">Space Tourism</h3>
+      <h5 className="text-h5 tracking-h5">Space Tourism</h5>
+      <p className="text-subheading-main">Space Tourism</p>
+      <p className="text-subheading-secondary tracking-subheading-secondary">
+        Space Tourism
+      </p>
+      <p className="text-nav tracking-nav">Space Tourism</p>
     </div>
   );
 }
